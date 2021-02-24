83840301439685fc1257305a51cd388067b5cef9
Ola Ishola <olaishola@hotmail.co.uk>
2021-02-24 21:49:52 +0100

    day 13 abstract class solution

---
+++ day_12_inheritance.js
@@ -0,0 +1,100 @@
+"use strict";
+
+var _input = "";
+var _index = 0;
+process.stdin.on("data", (data) => {
+    _input += data;
+});
+process.stdin.on("end", () => {
+    _input = _input.split(new RegExp("[ \n]+"));
+    main();
+});
+function read() {
+    return _input[_index++];
+}
+
+/**** Ignore above this line. ****/
+
+class Person {
+    constructor(firstName, lastName, identification) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
+
+    printPerson() {
+        console.log(
+            "Name: " +
+                this.lastName +
+                ", " +
+                this.firstName +
+                "\nID: " +
+                this.idNumber
+        );
+    }
+}
+
+class Student extends Person {
+    /*
+     *   Class Constructor
+     *
+     *   @param firstName - A string denoting the Person's first name.
+     *   @param lastName - A string denoting the Person's last name.
+     *   @param id - An integer denoting the Person's ID number.
+     *   @param scores - An array of integers denoting the Person's test scores.
+     */
+    // Write your constructor here
+    constructor(firstName, lastName, id, scores) {
+        super(firstName, lastName, id);
+        this.scores = scores;
+    }
+
+    gradeAvg() {
+        let avg =
+            this.scores.reduce((a, b) => {
+                return a + b;
+            }, 0) / this.scores.length;
+
+        return Math.floor(avg);
+    }
+    /*
+     *   Method Name: calculate
+     *   @return A character denoting the grade.
+     */
+    // Write your method here
+    calculate() {
+        let a = this.gradeAvg();
+        if (a > 90) {
+            return "O";
+        } else if (80 <= a && a < 90) {
+            return "E";
+        } else if (70 <= a && a < 80) {
+            return "A";
+        } else if (55 <= a && a < 70) {
+            return "P";
+        } else if (40 <= a && a < 55) {
+            return "D";
+        } else if (0 <= a && a < 40) {
+            return "T";
+        } else {
+            return "O";
+        }
+    }
+}
+
+function main() {
+    let firstName = read();
+    let lastName = read();
+    let id = +read();
+    let numScores = +read();
+    let testScores = new Array(numScores);
+
+    for (var i = 0; i < numScores; i++) {
+        testScores[i] = +read();
+    }
+
+    let s = new Student(firstName, lastName, id, testScores);
+    s.printPerson();
+    s.calculate();
+    console.log("Grade: " + s.calculate());
+}
