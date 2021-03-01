51f9956d73e3a7d8ef7e5f92facd2f208edf9116
Ola Ishola <olaishola@hotmail.co.uk>
2021-03-01 22:22:40 +0100

    day 16 throwing and catching exceptions

---
+++ day16_exception.js
@@ -0,0 +1,49 @@
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on("data", function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on("end", function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+/*
+ * You can use the following pseudocode:
+ *
+ * `S` = read string from stdin.
+ * try {
+ *     `N` = numeric value of `S`.
+ *
+ *     If `N` is `NaN` or `undefined`, then some exception must be thrown,
+ *     you can write the following withtout using conditional statement:
+ *     <condition to return true when `N` is `NaN` or `undefined`> && an_undefined_function_call()
+ *
+ *     print `N`
+ * } catch (err) {
+ *     print `Bad String`.
+ * }
+ */
+
+function main() {
+    const S = readLine();
+
+    try {
+        S != parseInt(S) && err();
+        console.log(parseInt(S));
+    } catch (e) {
+        console.log("Bad String");
+    }
+}
