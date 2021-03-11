1e913cccfbb245266ad7d37dc063028927291d1c
Ola Ishola <olaishola@hotmail.co.uk>
2021-03-11 22:53:18 +0100

    day 28 solution

---
+++ day_28_regex.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const N = parseInt(readLine(), 10);
+    
+    
+    const newArr = []
+    for (let NItr = 0; NItr < N; NItr++) {
+        const firstNameEmailID = readLine().split(' ');
+
+        const firstName = firstNameEmailID[0];
+
+        const emailID = firstNameEmailID[1];
+
+
+
+        let result = emailID.match(/@gmail.com/g)
+        if (result) {
+            newArr.push(firstName)
+        }
+
+    }
+
+    newArr.sort().forEach((name) => console.log(name))
+
+    
+}
